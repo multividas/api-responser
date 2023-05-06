5cabf28fb437940f965e38d2a60288a2750c0f51
Soulaimane yahya <soulaimaneyh07@gmail.com>
2023-05-06 17:03:30 +0100

    fixing php psr

--- src/Repositories/ApiRepository.php
+++ src/Repositories/ApiRepository.php
@@ -14,101 +14,101 @@ use Soulaimaneyh\ApiResponser\Interfaces\ApiRepositoryInterface;
 
 class ApiRepository implements ApiRepositoryInterface
 {
-	use ApiResponser;
+    use ApiResponser;
 
-	public function showAll(Collection $collection, $code = 200): JsonResponse
-	{
-		if ($collection->isEmpty()) {
-			return $this->successResponse(['data' => $collection], $code);
-		}
+    public function showAll(Collection $collection, $code = 200): JsonResponse
+    {
+        if ($collection->isEmpty()) {
+            return $this->successResponse(['data' => $collection], $code);
+        }
 
-		$collection = $this->filterData($collection);
-		$collection = $this->sortData($collection);
-		$collection = $this->paginate($collection);
-		$collection = $this->cacheResponse($collection);
+        $collection = $this->filterData($collection);
+        $collection = $this->sortData($collection);
+        $collection = $this->paginate($collection);
+        $collection = $this->cacheResponse($collection);
 
-		return $this->successResponse($collection, $code);
-	}
+        return $this->successResponse($collection, $code);
+    }
 
-	public function showOne(Model $instance, $code = 200): JsonResponse
-	{
-		return $this->successResponse([
-			'data' => $instance
-		], $code);
-	}
+    public function showOne(Model $instance, $code = 200): JsonResponse
+    {
+        return $this->successResponse([
+            'data' => $instance
+        ], $code);
+    }
 
-	protected function filterData(Collection $collection): Collection
-	{
-		if (isset(request()->query()['filters'])) {
-			foreach (request()->query()['filters'] as $query) {
-				$collection = $collection->where($query['field'], $query['value']);
-			}
-		}
+    protected function filterData(Collection $collection): Collection
+    {
+        if (isset(request()->query()['filters'])) {
+            foreach (request()->query()['filters'] as $query) {
+                $collection = $collection->where($query['field'], $query['value']);
+            }
+        }
 
-		return $collection;
-	}
+        return $collection;
+    }
 
-	protected function sortData(Collection $collection): Collection
-	{
-		if (request()->has('_sort')) {
-			$sortField = request()->_sort;
-			$sortOrder = request()->has('_order') && request()->_order == 'desc' ? TRUE : FALSE;
+    protected function sortData(Collection $collection): Collection
+    {
+        if (request()->has('_sort')) {
+            $sortField = request()->_sort;
+            $sortOrder = request()->has('_order') && request()->_order == 'desc' ? true : false;
 
-			$collection = $collection->sortBy($sortField, SORT_REGULAR, $sortOrder);
-		}
+            $collection = $collection->sortBy($sortField, SORT_REGULAR, $sortOrder);
+        }
 
-		return $collection;
-	}
+        return $collection;
+    }
 
-	/**
-	 * @return array|LengthAwarePaginator
-	 */
-	protected function paginate(Collection $collection)
-	{
-		$rules = [
-			'per_page' => 'integer|min:2|max:50',
-		];
+    /**
+     * @return array|LengthAwarePaginator
+     */
+    protected function paginate(Collection $collection)
+    {
+        $rules = [
+            'per_page' => 'integer|min:2|max:50',
+        ];
 
-		Validator::validate(request()->all(), $rules);
+        Validator::validate(request()->all(), $rules);
 
-		$perPage = 10;
+        $perPage = 10;
 
-		if (request()->has('per_page')) {
-			$perPage = (int) request()->per_page;
-		}
+        if (request()->has('per_page')) {
+            $perPage = (int) request()->per_page;
+        }
 
-		$page = LengthAwarePaginator::resolveCurrentPage();
-		$results = $collection->slice(($page - 1) * $perPage, $perPage)->values();
+        $page = LengthAwarePaginator::resolveCurrentPage();
+        $results = $collection->slice(($page - 1) * $perPage, $perPage)->values();
 
-		$paginated = new LengthAwarePaginator(
-			$results,
-			$collection->count(),
-			$perPage,
-			$page,
-			[
-				'path' => LengthAwarePaginator::resolveCurrentPath(),
-				'query' => request()->query()
-			]
-		);
+        $paginated = new LengthAwarePaginator(
+            $results,
+            $collection->count(),
+            $perPage,
+            $page,
+            [
+                'path' => LengthAwarePaginator::resolveCurrentPath(),
+                'query' => request()->query()
+            ]
+        );
 
-		$paginated->appends(request()->all());
+        $paginated->appends(request()->all());
 
-		return $paginated;
-	}
+        return $paginated;
+    }
 
-	protected function cacheResponse(array|LengthAwarePaginator $data): array
-	{
-		$url = request()->url();
-		$queryParams = request()->query();
+    protected function cacheResponse(array|LengthAwarePaginator $data): array
+    {
+        $url = request()->url();
+        $queryParams = request()->query();
 
-		ksort($queryParams);
+        ksort($queryParams);
 
-		$queryString = http_build_query($queryParams);
+        $queryString = http_build_query($queryParams);
 
-		$fullUrl = "{$url}?{$queryString}";
+        $fullUrl = "{$url}?{$queryString}";
 
-		return Cache::remember($fullUrl, 60, function () use ($data) {
-			return $data->toArray();
-		});
-	}
+        return Cache::remember($fullUrl, 60, function () use ($data) {
+            return $data->toArray();
+        });
+    }
 }
