687dfc57e19ae729858e5619ab89b5e160933ef2
Soulaimane yahya <soulaimaneyh07@gmail.com>
2023-05-22 22:38:01 +0100

    fixing testing

--- tests/Feature/ApiResponserTest.php
+++ tests/Feature/ApiResponserTest.php
@@ -3,6 +3,7 @@
 namespace Soulaimaneyh\ApiResponser\Tests\Feature;
 
 use Soulaimaneyh\ApiResponser\Tests\BaseTest;
+use Soulaimaneyh\ApiResponser\Tests\Models\User;
 use Soulaimaneyh\ApiResponser\Repositories\ApiRepository;
 
 class ApiResponserTest extends BaseTest
@@ -31,16 +32,48 @@ class ApiResponserTest extends BaseTest
 
         // assert response json
         $responseJson = $response->getContent();
-    
         $this->assertJson($responseJson);
+
         $responseData = json_decode($responseJson, true);
+
         $this->assertContains([
             'id' => 1,
             'name' => 'John'
         ], $responseData['data']);
+
         $this->assertContains([
             'id' => 2,
             'name' => 'Mark'
+        ], $responseData['data']);
+    }
+
+    public function testApiResponserShowOne()
+    {
+        $apiRepository = new ApiRepository();
+
+        $userArray = ['id' => 1, 'name' => 'john'];
+
+        // Convert the array to a User model instance
+        $userModel = User::hydrate([$userArray])->first();
+
+        $response = $apiRepository->showOne($userModel, 200);
+
+        // assert status response
+        $responseStatusCode = $response->getStatusCode();
+        $responseStatusText = $response->statusText();
+
+        $this->assertEquals($responseStatusCode, 200);
+        $this->assertEquals($responseStatusText, "OK");
+
+        // assert response json
+        $responseJson = $response->getContent();
+        $this->assertJson($responseJson);
+
+        $responseData = json_decode($responseJson, true);
+
+        $this->assertEquals([
+            'id' => 1,
+            'name' => 'john'
         ], $responseData['data']);
     }
 }
---
+++ tests/Models/User.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Soulaimaneyh\ApiResponser\Tests\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+    // Your custom code for the User model
+}
